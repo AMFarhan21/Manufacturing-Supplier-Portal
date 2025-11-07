3e4a2bdea344a93402fdd571af53d8d85d1b1e3c
AMFarhan <AMFarhan2102@gmail.com>
2025-11-07 12:06:30 +0800

    chore: delete commented code

--- service/xendit_service/xendit_repo.go
+++ service/xendit_service/xendit_repo.go
@@ -2,5 +2,4 @@ package xendit_service
 
 type XenditRepo interface {
 	XenditInvoiceUrl(userId, description, username, email, name, category string, paymentId int, amount float64) (string, error)
-	// XenditWebhook()
 }
